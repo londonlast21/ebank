7bf70e9ced8b7ace1bacc3dd39b82eaf40d995a3
jack <biffisbuffm@gmail.com>
2020-08-23 12:14:26 -0500

    npm start works

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
 
+require('dotenv').config();
+
 // check the input being used
-console.log(process.env.DB_USER, process.allowedNodeEnvironmentFlags.DB_PW);
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW);
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
