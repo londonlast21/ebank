52fb6400df8f800abe8380586b6b1acc5a46db4b
jack <biffisbuffm@gmail.com>
2020-08-22 23:33:57 -0500

    edits in serv

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,27 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // column 2: name of db
+    databaseName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // column 3 : mySql username
+    mySqlUsername: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // column 4 my sql password
+    mySqlPassword: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
