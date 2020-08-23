4e98a079cd813cc120dc82ba20438ba76a8eebb6
jack <biffisbuffm@gmail.com>
2020-08-23 11:06:51 -0500

    edits

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -98,17 +98,17 @@ router.delete('/:id', (req, res) => {
         id: req.params.id
       }
     })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-      res.json(dbPostData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+     .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 module.exports = router;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,13 +7,57 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id','product_name', 'price', 'stock'],
+    include: [{
+      model: Category,
+      attributes: [ 'id']
+    },
+    {
+      model: Tag,
+      attributes: ['id']
+    }
+    ]
+  }).then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include:[{
+      model: Category,
+      attributes: ['id']
+    },
+    {
+      model: Tag,
+      attributes: ['id']
+    }
+  ]
+  })
+  .then(dbPostData => {
+    if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
+
 
 // create new product
 router.post('/', (req, res) => {
