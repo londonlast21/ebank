7f3c95161671a82a5e235729da57abca8f7514c4
jack <biffisbuffm@gmail.com>
2020-08-23 17:06:33 -0500

    paths redo

--- models/Category.js
+++ models/Category.js
@@ -17,6 +17,8 @@ Category.init(
       type: DataTypes.STRING,
       allowNull: false,
     }
+  
+    
     
   },
   {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['product_name']
+        attributes: ['id', 'product_name']
       }
 
     ]
