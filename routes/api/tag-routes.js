1ed50893230c07255cae8164ecec35681a7b2fd7
jack <biffisbuffm@gmail.com>
2020-08-23 22:31:44 -0500

    almost

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -57,7 +57,8 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
   Tag.update(
     {
-    tag_name: req.body.tag_name
+      id: req.body.id,
+      tag_name: req.body.tag_name
     },
     {
       where: {
